87979ab5b887e399d105b0288c5c65ac7536ff32
Alex Dnl <deniel.alexandre@orange.fr>
2022-06-16 14:59:43 +0200

    final

--- src/test/java/com/crm/crm_spring/CrmSpringApplicationTests.java
+++ src/test/java/com/crm/crm_spring/CrmSpringApplicationTests.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest
 class CrmSpringApplicationTests {
